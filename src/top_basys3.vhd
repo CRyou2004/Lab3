d56d8bf66d4204d67f0ab590711668fad98dca7e
CRyou2004 <Chaimin2004@gmail.com>
2025-04-02 22:20:16 -0600

    Lab3Sub

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -59,21 +59,19 @@
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
-
-
+ 
+ 
 entity top_basys3 is
 	port(
-
+ 
 		clk     :   in std_logic; -- native 100MHz FPGA clock
-		
 		-- Switches (16 total)
 		sw  	:   in std_logic_vector(15 downto 0); -- sw(15) = left; sw(0) = right
-
+ 
 		-- LEDs (16 total)
 		-- taillights (LC, LB, LA, RA, RB, RC)
 		led 	:   out std_logic_vector(15 downto 0);  -- led(15:13) --> L
                                                         -- led(2:0)   --> R
-		
 		-- Buttons (5 total)
 		--btnC	:	in	std_logic
 		--btnU	:	in	std_logic;
@@ -82,25 +80,59 @@ entity top_basys3 is
 		--btnD	:	in	std_logic;	
 	);
 end top_basys3;
-
+ 
 architecture top_basys3_arch of top_basys3 is 
-  
 	-- declare components
-
-  
+component thunderbird_fsm is 
+    port (
+        i_clk, i_reset  : in    std_logic;
+        i_left, i_right : in    std_logic;
+        o_lights_L      : out   std_logic_vector(15 downto 13);
+        o_lights_R      : out   std_logic_vector(2 downto 0)
+     );
+  end component;   
+  component clock_divider is
+	generic ( constant k_DIV : natural := 2	);
+	port ( 	i_clk    : in std_logic;		   -- basys3 clk
+			i_reset  : in std_logic;		   -- asynchronous
+			o_clk    : out std_logic		   -- divided (slow) clock
+	);
+end component clock_divider;
+ 
+ 
+signal w_clk : std_logic;
+ 
 begin
 	-- PORT MAPS ----------------------------------------
-
-	
-	
+thunderbird_fsm_F : thunderbird_fsm
+port map(
+        i_clk => w_clk, 
+        i_reset => btnL, 
+        i_left => sw(15),
+        i_right => sw(0),
+        o_lights_L(15) =>   led(15),
+        o_lights_L(14) =>   led(14),
+        o_lights_L(13) =>   led(13),
+        o_lights_R(2) => led(2),
+        o_lights_R(1) => led(1),
+        o_lights_R(0) => led(0)
+ 
+	);
 	-- CONCURRENT STATEMENTS ----------------------------
-	
 	-- ground unused LEDs
 	-- leave unused switches UNCONNECTED
-	
+	   led(12 downto 3) <= (others => '0');
 	-- Ignore the warnings associated with these signals
 	-- Alternatively, you can create a different board implementation, 
 	--   or make additional adjustments to the constraints file
-	led(12 downto 3) <= (others => '0');
-	
+	--led(12 downto 3) <= (others => '0');
+
+--Complete the clock_divider portmap below based on the design provided	
+	clkdiv_inst : clock_divider 		--instantiation of clock_divider to take 
+        generic map ( k_DIV => 25000000 ) -- 1 Hz clock from 100 MHz
+        port map (						  
+            i_clk   => clk,
+            i_reset => btnR,
+            o_clk   => w_clk
+        );  
 end top_basys3_arch;
